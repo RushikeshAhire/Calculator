06201248e88948f1b9b07ad33b87a82af9dcb3ef
Rushikesh Ahire <rushiahire702@gmail.com>
2024-03-06 23:41:02 +0530

    Complete commit

---
+++ SIPCalculator/src/main/java/com/SIPcalculator/pojo/parameters.java
@@ -0,0 +1,35 @@
+package com.SIPcalculator.pojo;
+
+public class parameters {
+ private double principal;
+	 
+	 private double interest;
+	 
+	 private int noOfyears;
+
+	public double getPrincipal() {
+		return principal;
+	}
+
+	public void setPrincipal(double principal) {
+		this.principal = principal;
+	}
+
+	public double getInterest() {
+		return interest;
+	}
+
+	public void setInterest(double interest) {
+		this.interest = interest;
+	}
+
+	public int getNoOfyears() {
+		return noOfyears;
+	}
+
+	public void setNoOfyears(int noOfyears) {
+		this.noOfyears = noOfyears;
+	}
+
+
+}
